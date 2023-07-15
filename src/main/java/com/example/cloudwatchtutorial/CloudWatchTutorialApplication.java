884704cc5c5ef21443b184b1e6a25fc09159297d
Tetsuya Azami <zqianjian35@gmail.com>
2023-07-16 08:08:08 +0900

    cloudWatchにSpringBootアプリケーションからログを送信するtutorialを作成

---
+++ src/main/java/com/example/cloudwatchtutorial/CloudWatchTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudwatchtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudWatchTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudWatchTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cloudwatchtutorial/CloudWatchTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudwatchtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudWatchTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
